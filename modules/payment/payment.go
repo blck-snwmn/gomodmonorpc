8d7321ba2d900a049bbf12bb430c6a37f91371de
blck-snwmn <whitesnowmancreator@gmail.com>
2022-10-16 17:04:37 +0900

    Update payment

--- modules/payment/payment.go
+++ modules/payment/payment.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/blck-snwmn/gomodmonorpc/modules/invoker"
 	v1 "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/payment/v1"
+	"google.golang.org/grpc"
 )
 
 type (
@@ -20,6 +22,20 @@ func (oh *Payment) Do(ctx context.Context, input Input) (Output, error) {
 
 type PaymentService struct {
 	v1.UnimplementedPaymentServiceServer
+}
+
+func (ps *PaymentService) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, _ ...grpc.CallOption) error {
+	switch req := args.(type) {
+	case *v1.PayRequest:
+		resp, err := ps.Pay(ctx, req)
+		if err != nil {
+			return err
+		}
+		invoker.Cp(reply.(*v1.PayResponse), resp)
+		return nil
+	default:
+		panic("invalid args")
+	}
 }
 
 func (ps *PaymentService) Pay(context.Context, *v1.PayRequest) (*v1.PayResponse, error) {
