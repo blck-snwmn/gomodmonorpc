2d67846827ec60b88d60576017d415c02fd5b216
blck-snwmn <whitesnowmancreator@gmail.com>
2022-10-16 17:06:05 +0900

    Update grpc server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,31 +6,79 @@ import (
 	"log"
 	"net"
 
+	"github.com/blck-snwmn/gomodmonorpc/modules/invoker"
 	"github.com/blck-snwmn/gomodmonorpc/modules/order"
 	"github.com/blck-snwmn/gomodmonorpc/modules/payment"
 	myec "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/myec/v1"
+	orderv1 "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/order/v1"
+	paymentv1 "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/payment/v1"
 	"google.golang.org/grpc"
 )
 
-type server struct {
-	myec.UnimplementedMyECServiceServer
-	o *order.Order
-	p *payment.Payment
+type invokerx struct {
+	psv paymentv1.PaymentServiceServer
+	osv orderv1.OrderServiceServer
+	v   *invoker.Store
 }
 
-func (s *server) Order(context.Context, *myec.OrderRequest) (*myec.OrderResponse, error) {
-	_, err := s.o.Do(context.Background(), order.Input{})
+func (c *invokerx) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, options ...grpc.CallOption) error {
+	fmt.Printf("[%s]call\n", method)
+	defer fmt.Printf("[%s]end\n", method)
+
+	f, err := c.v.Invokef(method)
+	if err != nil {
+		return err
+	}
+	defer fmt.Printf("Invoke: %+[1]v, %[1]T\n", reply)
+	return f(ctx, method, args, reply, options...)
+}
+
+func (*invokerx) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	panic("unimplemented")
+}
+
+var _ grpc.ClientConnInterface = (*invokerx)(nil)
+
+type server struct {
+	myec.UnimplementedMyECServiceServer
+	pc paymentv1.PaymentServiceClient
+	oc orderv1.OrderServiceClient
+}
+
+func NewServer() *server {
+	psv := &payment.PaymentService{}
+	osv := &order.OrderService{}
+	s := invoker.NewStore()
+
+	s.RegisterByServiceName(orderv1.OrderService_ServiceDesc.ServiceName, osv.Invoke)
+	s.RegisterByServiceName(paymentv1.PaymentService_ServiceDesc.ServiceName, psv.Invoke)
+
+	iv := &invokerx{
+		psv: psv,
+		osv: osv,
+		v:   s,
+	}
+	return &server{
+		pc: paymentv1.NewPaymentServiceClient(iv),
+		oc: orderv1.NewOrderServiceClient(iv),
+	}
+}
+
+func (s *server) Order(ctx context.Context, req *myec.OrderRequest) (*myec.OrderResponse, error) {
+	resp, err := s.oc.Order(ctx, &orderv1.OrderRequest{})
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("Order: %+[1]v, %[1]T, Result=%q\n", resp, resp.Result)
 	return &myec.OrderResponse{}, nil
 }
 
-func (s *server) Payment(context.Context, *myec.OrderRequest) (*myec.OrderResponse, error) {
-	_, err := s.p.Do(context.Background(), payment.Input{})
+func (s *server) Payment(ctx context.Context, req *myec.OrderRequest) (*myec.OrderResponse, error) {
+	resp, err := s.pc.Pay(ctx, &paymentv1.PayRequest{})
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("Payment: %+[1]v, %[1]T, Result=%q\n", resp, resp.Result)
 	return &myec.OrderResponse{}, nil
 }
 
@@ -43,7 +91,7 @@ func main() {
 	}
 
 	sv := grpc.NewServer()
-	myec.RegisterMyECServiceServer(sv, &server{})
+	myec.RegisterMyECServiceServer(sv, NewServer())
 	log.Println("Listening on", listenOn)
 	if err := sv.Serve(listener); err != nil {
 		fmt.Printf("failed to serve gRPC server: %+v", err)
--- go.mod
+++ go.mod
@@ -3,17 +3,18 @@ module github.com/blck-snwmn/gomodmonorpc
 go 1.19
 
 require (
-	github.com/blck-snwmn/gomodmonorpc/modules/order v0.0.0-20221015101017-798aa7f903e8
-	github.com/blck-snwmn/gomodmonorpc/modules/payment v0.0.0-20221015101017-798aa7f903e8
-	github.com/blck-snwmn/gomodmonorpc/modules/proto v0.0.0-20221015190110-e563187809a6
+	github.com/blck-snwmn/gomodmonorpc/modules/invoker v0.0.0-20221016063323-614caffa25a6
+	github.com/blck-snwmn/gomodmonorpc/modules/order v0.0.0-20221016063323-614caffa25a6
+	github.com/blck-snwmn/gomodmonorpc/modules/payment v0.0.0-20221016063323-614caffa25a6
+	github.com/blck-snwmn/gomodmonorpc/modules/proto v0.0.0-20221016063323-614caffa25a6
 	google.golang.org/grpc v1.50.1
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
+	golang.org/x/text v0.3.8 // indirect
+	google.golang.org/genproto v0.0.0-20221014213838-99cd37c6964a // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
