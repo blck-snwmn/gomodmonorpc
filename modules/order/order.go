d338bcd0203f8c9efd3d83d719ff09645cb2c41e
blck-snwmn <whitesnowmancreator@gmail.com>
2022-10-16 17:04:30 +0900

    Update order.go

--- modules/order/order.go
+++ modules/order/order.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/blck-snwmn/gomodmonorpc/modules/invoker"
 	v1 "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/order/v1"
+	"google.golang.org/grpc"
 )
 
 type (
@@ -20,6 +22,20 @@ func (oh *Order) Do(ctx context.Context, input Input) (Output, error) {
 
 type OrderService struct {
 	v1.UnimplementedOrderServiceServer
+}
+
+func (os *OrderService) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, _ ...grpc.CallOption) error {
+	switch req := args.(type) {
+	case *v1.OrderRequest:
+		resp, err := os.Order(ctx, req)
+		if err != nil {
+			return err
+		}
+		invoker.Cp(reply.(*v1.OrderResponse), resp)
+		return nil
+	default:
+		panic("invalid args")
+	}
 }
 
 func (os *OrderService) Order(context.Context, *v1.OrderRequest) (*v1.OrderResponse, error) {
