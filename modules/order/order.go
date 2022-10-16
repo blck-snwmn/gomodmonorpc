018c81ad74938e5995139991e20ebf820fffe47f
blck-snwmn <whitesnowmancreator@gmail.com>
2022-10-16 13:56:45 +0900

    Add grpc server

--- modules/order/order.go
+++ modules/order/order.go
@@ -1,6 +1,11 @@
 package order
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	v1 "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/order/v1"
+)
 
 type (
 	Input  struct{}
@@ -11,4 +16,15 @@ type Order struct{}
 
 func (oh *Order) Do(ctx context.Context, input Input) (Output, error) {
 	return Output{}, nil
+}
+
+type OrderService struct {
+	v1.UnimplementedOrderServiceServer
+}
+
+func (os *OrderService) Order(context.Context, *v1.OrderRequest) (*v1.OrderResponse, error) {
+	fmt.Println("call: Order")
+	return &v1.OrderResponse{
+		Result: "ok",
+	}, nil
 }
--- modules/payment/payment.go
+++ modules/payment/payment.go
@@ -1,6 +1,11 @@
 package payment
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	v1 "github.com/blck-snwmn/gomodmonorpc/modules/proto/gen/grpc/payment/v1"
+)
 
 type (
 	Input  struct{}
@@ -11,4 +16,15 @@ type Payment struct{}
 
 func (oh *Payment) Do(ctx context.Context, input Input) (Output, error) {
 	return Output{}, nil
+}
+
+type PaymentService struct {
+	v1.UnimplementedPaymentServiceServer
+}
+
+func (ps *PaymentService) Pay(context.Context, *v1.PayRequest) (*v1.PayResponse, error) {
+	fmt.Println("call: Pay")
+	return &v1.PayResponse{
+		Result: "ok",
+	}, nil
 }
